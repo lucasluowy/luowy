6e4b2a1404263fa543f10559e696c86374cbc3a4
罗文勇 <luowenyong@chehang168.com>
2019-07-02 15:47:13 +0800

    lwySdk

---
+++ demp.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once './vendor/autoload.php';
+use luowySdk\luowy\luowenyong\Tools;
+
+Tools::Hello();
+
+
---
+++ src/luowy/luowenyong/Tools.php
@@ -0,0 +1,13 @@
+<?php
+//namespace luowy\luowenyong;
+namespace luowySdk\luowy\luowenyong;
+
+class Tools{
+    public function __construct(){
+        
+    }
+
+    public static function Hello(){
+        echo "Hellp World";
+    }
+}
